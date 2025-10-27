c3c4ff46db409f07d5de258a2890331cad2833aa
Lindsay Fabricant <lindsayfabricant@gmail.com>
2025-10-27 17:51:31 -0400

    Initial Commit

---
+++ AMAL/AMALApp.swift
@@ -0,0 +1,17 @@
+//
+//  AMALApp.swift
+//  AMAL
+//
+//  Created by Lindsay on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct AMALApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
